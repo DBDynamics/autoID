9888fd1911dbe03c06212e1670aca235ae4e8915
DBDynamics <wade_cool@126.com>
2025-09-18 11:20:24 +0800

    update

--- autoID_v3s.py
+++ autoID_v3s.py
@@ -1,17 +1,8 @@
 # 这个程序实现自动修改ID的功能
-from DBDynamics import Bee
+from limbpro import Bee
 import time
-m = Bee('COM9')
+m = Bee()
 
-def play_voice(content):
-    try:
-        import pyttsx3
-        engine = pyttsx3.init()
-        engine.say(content)
-        engine.runAndWait()
-    except ImportError:
-        print("请安装 pyttsx3 库以使用语音播放功能。")
-        
 startID = 1
 for target in range(startID, 18, 1):
     while True:
@@ -19,7 +10,7 @@ for target in range(startID, 18, 1):
         ret = m.getDeviceType(0)
         if ret !=0:
             print("Device 0 is connected")
-            play_voice("新设备已连接")
+            print("新设备已连接")
             break
     currentID  = 0
     targetID = target
@@ -30,16 +21,16 @@ for target in range(startID, 18, 1):
     if ret!=0:
         print("Device ", targetID, " is ready")
         msg = str(targetID) + "号设备修改成功"
-        play_voice(msg)
+        print(msg)
 
-# 播放全部修改成功的语音
+# 输出全部修改成功的信息
 msg = "全部设备修改成功"
-play_voice(msg)
+print(msg)
 
 l = m.scanDevices()
 ll = len(l)
 print("Total devices: ", ll)
 msg = "共扫描到" + str(ll) + "个设备"
-play_voice(msg)
+print(msg)
 
 m.stop()
